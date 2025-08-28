114ca7e78d15d7e67992025aa62439301770db21
Brian Poon <86692163+Brian-S-Poon@users.noreply.github.com>
2025-08-27 21:56:24 -0700

    Add email link to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,8 +8,15 @@ export default function Footer() {
       <p className="mt-2 space-x-2">
         <Link to="/now" className="underline hover:text-blue-500 dark:hover:text-blue-400">Now</Link> ·
         <Link to="/blog" className="underline hover:text-blue-500 dark:hover:text-blue-400">Blog</Link> ·
-        <Link to="/projects" className="underline hover:text-blue-500 dark:hover:text-blue-400">Projects</Link>
+        <Link to="/projects" className="underline hover:text-blue-500 dark:hover:text-blue-400">Projects</Link> ·
+        <a
+          href="mailto:brian@brianpoon.com"
+          className="underline hover:text-blue-500 dark:hover:text-blue-400"
+        >
+          Email
+        </a>
       </p>
+      {/* TODO: Consider obfuscating the email or using a contact form to reduce spam */}
     </footer>
   );
 }
